ac64876c60d23343dc49d87f0259caed4faaea7d
SophonAlpha <SophonAlpha@users.noreply.github.com>
2018-05-16 21:07:30 +0400

    save

--- format_a_string_of_names.py
+++ format_a_string_of_names.py
@@ -1,5 +1,13 @@
 def namelist(names):
-    
+    ret_str = ''
+    s = [e['name'] for e in names]
+    if len(s) > 1:
+        ret_str = ', '.join(s[:-1]) + ' & ' + s[-1]
+    if len(s) == 1:
+        ret_str = s[0]
+    return ret_str
 
-
-print(namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ]))
+print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]))
+print(namelist([{'name': 'Bart'},{'name': 'Lisa'}]))
+print(namelist([{'name': 'Bart'}]))
+print(namelist([]))
