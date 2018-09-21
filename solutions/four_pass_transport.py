c45ffa94fdb5339a05d97ec81903944f823d9a9b
SophonAlpha <SophonAlpha@users.noreply.github.com>
2018-09-21 18:02:45 +0400

    added distance calculation, added sequence creation

--- solutions/four_pass_transport.py
+++ solutions/four_pass_transport.py
@@ -11,7 +11,7 @@ import itertools
 
 CONV_BELT_MOD = 9 # a numerical representation of one conveyer belt module
 
-class FactoryFloor:
+class Factory:
     """
     Represent the factory floor as an object.
     """
@@ -19,6 +19,7 @@ class FactoryFloor:
     def __init__(self, stations):
         self.floor = [[None] * 10 for _ in range(0, 10)]
         self.stations = {}
+        self.occupied = set()
         self.place_stations(stations)
 
     def place_stations(self, stations):
@@ -31,12 +32,25 @@ class FactoryFloor:
             col = station % 10
             self.floor[row][col] = station_num
             self.stations[station_num] = (row, col)
+            self.mark_occupied([(row, col)])
 
     def place_conveyer_belt(self, row, col):
         """
         Place one conveyer belt module on the factory floor.
         """
         self.floor[row][col] = CONV_BELT_MOD
+        self.mark_occupied([(row, col)])
+
+    def mark_occupied(self, tiles):
+        for row, col in tiles:
+            self.occupied.add((row, col))
+
+    def unmark_occupied(self, tiles):
+        for row, col in tiles:
+            self.occupied.discard((row, col))
+
+    def is_occupied(self, row, col):
+        return (row, col) in self.occupied
 
     def show_floor(self):
         """
@@ -50,22 +64,22 @@ class FactoryFloor:
 
 class PathPlanner:
     
-    def __init__(self, factory_floor):
-        self.factory_floor = factory_floor
-        self.occupied_tiles = list(self.factory_floor.stations.values())
+    def __init__(self, factory):
+        self.factory = factory
+        self.occupied_tiles = list(self.factory.stations.values())
 
-    def get_shortest_path(self, start_tile, target_row, target_col):
+    def get_shortest_path(self, start_tile, end_tile):
         """
         Calculate a sequence of tiles to be moved that get a tile to the target
         position. Uses Dijkstra algorithm for shortest path calculation.
         """
         graph = self.build_graph(start_tile, end_tile)
         distances = self.calculate_distances(start_tile, graph)
-        tile_sequence = self.get_shortest_tile_sequence(distances, graph,
-                                                        start_tile,
-                                                        target_row, target_col)
-        tile_sequence.reverse()
-        return tile_sequence
+#         tile_sequence = self.get_shortest_tile_sequence(distances, graph,
+#                                                         start_tile,
+#                                                         target_row, target_col)
+#         tile_sequence.reverse()
+#         return tile_sequence
 
     def build_graph(self, start_tile, end_tile):
         """
@@ -75,26 +89,27 @@ class PathPlanner:
         building the graph. Stations except the target station are excluded as 
         well. This way the algorithm calculates paths around occupied tiles.
         """
-        floor_dim = len(self.factory_floor.floor)
-        self.unmark_occupied([start_tile, end_tile])
+        floor_dim = len(self.factory.floor)
+        self.factory.unmark_occupied([start_tile, end_tile])
+        tiles = set(itertools.product(range(0, floor_dim), repeat=2))
+        tiles = tiles - self.factory.occupied
         distance = 1
         graph = {}
-        for row, col in itertools.product(range(0,floor_dim), repeat=2):
-            surrounding_tiles = self.get_surrounding_tiles(row, col)
-            for n_row, n_col in surrounding_tiles:
-                if not self.factory_floor.floor[n_row][n_col] == CONV_BELT_MOD and \
-                not 
-        
-
+        for row, col in tiles:
+            n_tiles = self.get_neighbour_tiles(row, col)
+            distances = {(n_row, n_col): distance for n_row, n_col in n_tiles \
+                         if not self.factory.is_occupied(n_row, n_col)}
+            graph[(row, col)] = distances
+        self.factory.mark_occupied([start_tile, end_tile])
         return graph
 
-    def get_surrounding_tiles(self, row, col):
+    def get_neighbour_tiles(self, row, col):
         """
         Get all tiles around a given tile.
         """
         positions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        max_row = len(self.factory_floor.factory_floor) - 1
-        max_col = len(self.factory_floor.factory_floor[0]) - 1
+        max_row = len(self.factory.floor) - 1
+        max_col = len(self.factory.floor[0]) - 1
         neighbours = []
         for d_row, d_col in positions:
             n_row = row + d_row
@@ -104,31 +119,15 @@ class PathPlanner:
                 neighbours.append((n_row, n_col))
         return neighbours
 
-
-
-
-
-
-        t_row, t_col = self.get_position(tile)
-        sequence = []
-        for d_row, d_col in positions:
-            n_row = t_row + d_row
-            check_col = t_col + d_col
-            if n_row >= 0 and n_row <= self.puzzle_size - 1 and \
-               check_col >= 0 and check_col <= self.puzzle_size - 1:
-                sequence.append(self.tile_array[n_row][check_col])
-            else:
-                sequence.append(None)
-        return sequence
-
     def calculate_distances(self, start_tile, graph):
         """
         Perform the distance calculations required for the Dijkstra algorithm.
+        
+        Based on this introduction to the Dijkstra algorithm:
+        https://brilliant.org/wiki/dijkstras-short-path-finder/
         """
-        # based on this introduction to the Dijkstra algorithm:
-        # https://brilliant.org/wiki/dijkstras-short-path-finder/
         infinity = float('inf')
-        unvisited = [vertex for vertex in graph if not self.untouchable[vertex]]
+        unvisited = [vertex for vertex in graph]
         distances = {vertex: infinity for vertex in unvisited}
         distances[start_tile] = 0
         # calculate distances from source
@@ -149,7 +148,8 @@ class PathPlanner:
         have been calculated, this function works out the sequence of tiles
         to be moved.
         """
-        tile = self.tile_array[target_row][target_col]
+        tile = None # dummy, remove 
+#         tile = self.tile_array[target_row][target_col]
         tile_sequence = []
         while not tile == start_tile:
             neighbor_dist = {neighbor:distances[neighbor] for neighbor in graph[tile]}
@@ -157,15 +157,11 @@ class PathPlanner:
             tile = min(neighbor_dist, key=neighbor_dist.get)
         return tile_sequence
 
-
-
-
-
 def four_pass(stations):
     """ main function """
-    factory_floor = FactoryFloor(stations)
+    factory_floor = Factory(stations)
     factory_floor.show_floor()
     path_planner = PathPlanner(factory_floor)
-    
+    path_planner.get_shortest_path((0, 1), (6, 9))
 
 four_pass([1, 69, 95, 70])
