7d62dea6d46abccdc8f969e8407d4af066d23b1e
SophonAlpha <SophonAlpha@users.noreply.github.com>
2018-10-28 22:18:48 +0400

    finding patterns in xor oparation

--- solutions/become_immortal.py
+++ solutions/become_immortal.py
@@ -7,5 +7,32 @@ Level: 1 kyu
 
 """
 
+import numpy as np
+
+np.set_printoptions(threshold=np.inf)
+
 def elder_age(m, n, l, t):
-    return None
+    rows, cols = np.array(np.meshgrid(np.arange(m), np.arange(n))).reshape(2, m*n)
+    xor_arr = np.bitwise_xor(rows, cols).reshape(n, m)
+    trans_loss = np.subtract(xor_arr, l)
+    trans_loss[trans_loss < 0] = 0
+    np.savetxt("trans_loss.csv", trans_loss, fmt='%i', delimiter=",")
+    print(trans_loss)
+    donate_time = np.sum(trans_loss) % t
+    print('donate_time = {}'.format(donate_time))
+    return donate_time
+
+def sum_array():
+    max_m, max_n, l, t = 40, 40, 0, 10000
+    sum_arr = np.zeros(shape=(max_n, max_m))
+    input_arr = np.array(np.meshgrid(np.arange(max_n),
+                                     np.arange(max_m))).reshape(2, max_n*max_m).T
+    for _, (n, m) in enumerate(input_arr):
+        donate_time = elder_age(m, n, l, t)
+        sum_arr[n, m] = donate_time
+    print(sum_arr)
+
+if __name__ == "__main__":
+    elder_age(48, 48, 0, 10000)
+#     sum_array()
+    
--- tests/test_become_immortal.py
+++ tests/test_become_immortal.py
@@ -9,7 +9,14 @@ import pytest
 
 from solutions.become_immortal import elder_age
 
-TESTS = [(8, 5, 1, 100, 5)]
+TESTS = [(8, 5, 1, 100, 5),
+         (7, 4, 1, 100, 66),
+         (8, 8, 0, 100007, 224),
+         (25, 31, 0, 100007, 11925),
+         (5, 45, 3, 1000007, 4323),
+         (31, 39, 7, 2345, 1586),
+         (545, 435, 342, 1000007, 808451)]
+#          (28827050410, 35165045587, 7109602, 13719506, 5456283)]
 
 @pytest.mark.parametrize('m, n, l, t, correct_age', TESTS)
 def test_cases(m, n, l, t, correct_age):
