c6f8925443c87eb686549dc76ad3a7fecf5210ef
SophonAlpha <SophonAlpha@users.noreply.github.com>
2018-11-01 08:55:17 +0400

    build general approach for larger arrays

--- solutions/become_immortal.py
+++ solutions/become_immortal.py
@@ -23,23 +23,63 @@ def elder_age(m, n, l, t):
     
     8**12 = 68,719,476,736
     
-    tile_delta = np.array([[0, 1, 2, 3, 4, 5, 6, 7],
-                           [1, 0, 3, 2, 5, 4, 7, 6],
-                           [2, 3, 0, 1, 6, 7, 4, 5],
-                           [3, 2, 1, 0, 7, 6, 5, 4],
-                           [4, 5, 6, 7, 0, 1, 2, 3],
-                           [5, 4, 7, 6, 1, 0, 3, 2],
-                           [6, 7, 4, 5, 2, 3, 0, 1],
-                           [7, 6, 5, 4, 3, 2, 1, 0]])
-
+    base_tile = np.array([[0, 1, 2, 3, 4, 5, 6, 7],
+                          [1, 0, 3, 2, 5, 4, 7, 6],
+                          [2, 3, 0, 1, 6, 7, 4, 5],
+                          [3, 2, 1, 0, 7, 6, 5, 4],
+                          [4, 5, 6, 7, 0, 1, 2, 3],
+                          [5, 4, 7, 6, 1, 0, 3, 2],
+                          [6, 7, 4, 5, 2, 3, 0, 1],
+                          [7, 6, 5, 4, 3, 2, 1, 0]])
+    
+    8**1 x 8**1 =  8 x  8 cells =
+    8x8 = origin_tile = base_tile
+    
+    8**2 x 8**2 = 64 x 64 cells =
+    tile_index = 0 - 7
+    t[tile_index] = (sum(origin_tile) + \        the sum of xors of base tile originating at 0, 0
+                     tile_index * 8 * 8 * 8 - \  adder that represents the sum to be added for tiles along columns
+                                                ' * 8'     - tile width by which to shift to the right
+                                                ' * 8 * 8' - sum for tile with given dimensions where each cell 
+                                                             value is equal to the tile width
+                                                             
+                                                 
+                     l * 8 * 8 + \               the amount of loss for the whole tile
+                     4 * l * (l + 1)) \          the sum of all values < 0 in a tile
+                     % t                         max age
+    tile_time = t[0] * 8 + \
+                t[1] * 8 + \
+                t[2] * 4 + \
+                t[3] * 8 + \
+                t[4] * 8 + \
+                t[5] * 8 + \
+                t[6] * 4 + \
+                t[7] * 8
     """
+
+
+origin_tile = np.array([[0, 1, 2, 3, 4, 5, 6, 7],
+                        [1, 0, 3, 2, 5, 4, 7, 6],
+                        [2, 3, 0, 1, 6, 7, 4, 5],
+                        [3, 2, 1, 0, 7, 6, 5, 4],
+                        [4, 5, 6, 7, 0, 1, 2, 3],
+                        [5, 4, 7, 6, 1, 0, 3, 2],
+                        [6, 7, 4, 5, 2, 3, 0, 1],
+                        [7, 6, 5, 4, 3, 2, 1, 0]])
 
 def tile_group(m, n, l, t):
     q, r = divmod(max(m, n), 8)
     tile_index = 0
     while tile_index < q:
+        # the sum of xors of tile originating at relative position 0, 0
+        xor_sum = np.sum(origin_tile)
+        
+        col_adder = tile_index * 8 * 8 * 8
+        
+        
+        
         tile_time = (224 + \
-                     tile_index * 512 - \
+                     tile_index * 8 * 8 * 8 - \
                      l * 64 + \
                      4 * l * (l + 1)) % t
         print(tile_time)
