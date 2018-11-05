18d2a0fe0ab7cb15269e1bcbb52c2c85fc64f305
SophonAlpha <SophonAlpha@users.noreply.github.com>
2018-11-05 22:39:39 +0400

    approach with mod 8

--- solutions/become_immortal.py
+++ solutions/become_immortal.py
@@ -19,11 +19,10 @@ def elder_age(m, n, l, t):
     donate_time += xor_time_sum(m2_s, m2_e, n2_s, n2_e)
     m3_s, n3_s = m1_e, m1_e
     m3_e, n3_e = m3_s + m1_e, n
-    donate_time += xor_time_sum(m2_s, m2_e, n2_s, n2_e)
-    m4_s, n4_s = m1_e + m1_e, m1_e
-    m4_e, n4_e = m3_s + m1_e, n
-    
-    
+    donate_time += xor_time_sum(m3_s, m3_e, n3_s, n3_e)
+    m4_s, n4_s = m1_e, m1_e
+    m4_e, n4_e = m, n
+    donate_time += tile(m4_s, m4_e, n4_s, n4_e, l, t)
     return donate_time
 
 def xor_time_sum(m_s, m_e, n_s, n_e):
@@ -74,8 +73,8 @@ def tile_time(m, n, l, t, origin, start_col, level):
     return time
 
 
-def tile(m, n, l, t):
-    rows, cols = np.array(np.meshgrid(np.arange(n), np.arange(m))).reshape(2, -1)
+def tile(m_s, m_e, n_s, n_e, l, t):
+    rows, cols = np.array(np.meshgrid(np.arange(n_s, n_e), np.arange(m_s, m_e))).reshape(2, -1)
     xor_arr = np.bitwise_xor(rows, cols)
     trans_loss = np.subtract(xor_arr, l)
     trans_loss[trans_loss < 0] = 0
@@ -89,7 +88,7 @@ if __name__ == "__main__":
     m, n, l, t = 545, 435, 342, 1000007
     m, n, l, t = 64, 64, 0, 100000
     m, n, l, t = 1024, 512, 0, 100000
-    m, n, l, t = 64, 88, 0, 100000
     m, n, l, t = 67, 67, 0, 100000
+    m, n, l, t = 64, 88, 0, 100000
     print(elder_age(m, n, l, t))
     print(tile(m, n, l, t**2))
