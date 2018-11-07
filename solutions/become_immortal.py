fix sub tile calculation

solutions/become_immortal.py
@@ -24,19 +24,6 @@ def elder_age(m, n, l, t):
     m1 = m if m < 8**level else divmod(m, 8**level)[0] * 8**level
     n1 = n if n < 8**level else divmod(n, 8**level)[0] * 8**level
     donate_time += tile_time(m1, n1, l, t, 0, 0, level)
-
-    """
-	level 0:
-		origin = 0
-		(0, 0) = origin
-		(0, column) = column * 8**level * 8**level + (0, 0)
-
-	level 1 (8):
-		origin = (8x8 sum of level 0)
-		(0, column) = column * 8**level * 8**level + (0, 0)
-
-	"""
-
     return donate_time
 
 def xor_time_sum(m_s, m_e, n_s, n_e):
@@ -63,22 +50,12 @@ def tile_time(m, n, l, t, origin, start_col, level):
         xor_arr = np.add(np.multiply(index, 8**level * 8**level), origin)
     else:
         square_size = 8**(level - 1)
-        m_quo, _ = divmod(m, square_size)
-        n_quo, _ = divmod(n, square_size)
+        m_quo = divmod(m, square_size)[0] if m < 8**level else 8**level
+        n_quo = divmod(n, square_size)[0] if n < 8**level else 8**level
         xor_arr = np.add(np.multiply(index, m_quo * square_size * n_quo * square_size), origin)
-    val_row = 1 if m < 8**level else divmod(m, 8**level)[0]
-    val_col = 1 if n < 8**level else divmod(n, 8**level)[0]
+    val_col = 1 if m < 8**level else divmod(m, 8**level)[0]
+    val_row = 1 if n < 8**level else divmod(n, 8**level)[0]
     time = np.sum(xor_arr[VALUES[:val_row, :val_col]])
-    
-#     m_count = m if level == 0 else 8**(level - 1)
-#     n_count = n if level == 0 else 8**(level - 1)
-#     quo, rem = divmod(m, 8**(level + 1))
-#     if quo == 0 and level == 0:
-#         time = tile(0, m, 0, n, l, t)
-#     elif quo == 0 and level > 0:
-#         pass
-#     else:
-#         time = m * ((start_col + start_col + m - 1) / 2) * n
     return time
 
 def tile(m_s, m_e, n_s, n_e, l, t):
@@ -97,8 +74,9 @@ if __name__ == "__main__":
     m, n, l, t = 64, 64, 0, 100000
     m, n, l, t = 1024, 512, 0, 100000
     m, n, l, t = 67, 67, 0, 100000
-    m, n, l, t = 19, 53, 0, 100000
     m, n, l, t = 5, 6, 0, 100000
     m, n, l, t = 5, 16, 0, 100000
+    m, n, l, t = 19, 53, 0, 100000
+    m, n, l, t = 19, 83, 0, 100000
     print(elder_age(m, n, l, t))
     print(tile(m, n, l, t**2))