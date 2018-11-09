correct calculation for arrays with size 8**1 and higher levels

solutions/become_immortal.py
@@ -61,7 +61,7 @@ def tile_time(m_start, m_end, n_start, n_end, origin, level, l, t):
             sub_n_end = sub_n_start + sub_m_end - sub_m_start
         else:
             sub_n_end = sub_n_start + n_end
-        origin = tile_time(sub_m_start, sub_m_end, sub_n_start, sub_n_end,
+        origin = tile_time(0, sub_m_end - sub_m_start, 0, sub_n_end - sub_n_start,
                            origin, level - 1, l, t)
     index = np.arange(m_start, m_start + 8**(level + 1), 8**level)
     dm = m_end - m_start
@@ -76,7 +76,7 @@ def tile_time(m_start, m_end, n_start, n_end, origin, level, l, t):
         # 8**0 = 1x1 = 1
         # 8**1 = 8x8 = 512
         # 8**2 = 64x64 = 262144
-        base_origin = tile_time(0, 8**level, 0, 8**level, 0, level - 1, l, t)
+#         base_origin = tile_time(0, 8**level, 0, 8**level, 0, level - 1, l, t)
         xor_arr = np.add(np.multiply(index, m_quo * square_size * n_quo * square_size), origin)
     val_col = 1 if dm < 8**level else divmod(dm, 8**level)[0]
     val_row = 1 if dn < 8**level else divmod(dn, 8**level)[0]