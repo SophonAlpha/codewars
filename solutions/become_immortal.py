ec376bfe1562509b851acdca2d7b82726aeda3ec
SophonAlpha <SophonAlpha@users.noreply.github.com>
2018-12-31 00:45:13 +0400

    add performance measurement

--- solutions/become_immortal.py
+++ solutions/become_immortal.py
@@ -8,7 +8,11 @@ Level: 1 kyu
 """
 import numpy as np
 np.seterr(over='raise')
+from solutions.performance import Profile
 
+PERFORMANCE_STATS = []
+
+@Profile(stats=PERFORMANCE_STATS)
 def elder_age(m, n, l, t):
     total_time = 0
     total_loss = 0
@@ -16,10 +20,8 @@ def elder_age(m, n, l, t):
     m_start, n_start = 0, 0
     m_end, n_end = m, n
     while n_start != n_end:
-        dm = m_end - m_start
-        dn = n_end - n_start
-        m_level = largest_sqare_tile(dm)
-        n_level = largest_sqare_tile(dn)
+        dm, dn = m_end - m_start, n_end - n_start
+        m_level, n_level = largest_sqare_tile(dm), largest_sqare_tile(dn)
         dm = divmod(dm, 8**m_level)[0] * 8**m_level
         dn = divmod(dn, 8**n_level)[0] * 8**n_level
         cell_value = m_start ^ n_start
@@ -37,6 +39,7 @@ def elder_age(m, n, l, t):
     donate_time = donate_time % t
     return total_time, donate_time
 
+@Profile(stats=PERFORMANCE_STATS)
 def largest_sqare_tile(size):
     exp = 1
     if size < 8**exp:
@@ -48,34 +51,35 @@ def largest_sqare_tile(size):
         exp += 1
     return exp
 
-def tile_time(m_start, dm, n_start, dn, sub_sum, level):
+@Profile(stats=PERFORMANCE_STATS)
+def tile_time(m_start, dm, n_start, dn, cell_value, level):
     if dm < 1 or dn < 1:
         return 0
-    if dn > dm:
-        m_start, dm, n_start, dn = n_start, dn, m_start, dm
+    m_start, dm, n_start, dn = width_bigger_than_height(m_start, dm, n_start, dn)
     m_values = range(m_start, m_start + 8**(level + 1), 8**level)
-    xor_arr = [value ^ n_start 
-               for value in m_values]
+    xor_arr = [value ^ n_start for value in m_values]
     positions = [(value - min(xor_arr)) // 8**level for value in xor_arr]
     if level > 0:
         sub_m_start = m_start + xor_arr.index(min(xor_arr)) * 8**level
-        sub_sum = sub_tile_time(dm, dn, sub_m_start, n_start, sub_sum, level)
+        cell_value = sub_tile_time(sub_m_start, dm, n_start, dn,
+                                   cell_value, level)
     else:
-        sub_sum = m_start ^ n_start
+        cell_value = m_start ^ n_start
     if dm >= 8**level:
         rows = min(dn, 8**level)
         delta = (sum_seq(8**level, 8**level + 8**level - 1) - \
                  sum_seq(0, 8**level - 1)) * rows
-        xor_arr = [(value * delta) + sub_sum for value in positions]
+        xor_arr = [(value * delta) + cell_value for value in positions]
         val_col = divmod(dm, 8**level)[0]
         val_row = max(1, divmod(dn, 8**level)[0])
         xor_arr = map_row_to_array(xor_arr, val_row, val_col)
         time = sum([sum(row) for row in xor_arr])
     else:
-        time = sub_sum
+        time = cell_value
     return time, xor_arr
 
-def sub_tile_time(dm, dn, m_start, n_start, sub_sum, level):
+@Profile(stats=PERFORMANCE_STATS)
+def sub_tile_time(m_start, dm, n_start, dn, sub_sum, level):
     if level > 1:
         m_values = range(m_start, m_start + 8**level, 8**(level - 1))        
         xor_arr = [value ^ n_start for value in m_values]
@@ -84,7 +88,7 @@ def sub_tile_time(dm, dn, m_start, n_start, sub_sum, level):
         sub_dm = min(dm, 8**level)
         sub_dn = min(dn, 8**level)
         sub_m_start = m_start + xor_arr.index(min(xor_arr)) * 8**(level - 1)
-        sub_sum = sub_tile_time(sub_dm, sub_dn, sub_m_start, n_start, 
+        sub_sum = sub_tile_time(sub_m_start, sub_dm, n_start, sub_dn, 
                                 sub_sum, level - 1)
     else:
         m_values = range(m_start, m_start + 8)
@@ -101,6 +105,7 @@ def sub_tile_time(dm, dn, m_start, n_start, sub_sum, level):
     time = sum(xor_sums) * tile_rows
     return time
 
+@Profile(stats=PERFORMANCE_STATS)
 def map_row_to_array(first_row, num_rows, num_cols):
     first_row_to_8x8_map = [[0, 1, 2, 3, 4, 5, 6, 7],
                             [1, 0, 3, 2, 5, 4, 7, 6],
@@ -117,17 +122,24 @@ def map_row_to_array(first_row, num_rows, num_cols):
         array.append(new_row)
     return array
 
+@Profile(stats=PERFORMANCE_STATS)
+def width_bigger_than_height(m_start, dm, n_start, dn):
+    if dn > dm:
+        m_start, dm, n_start, dn = n_start, dn, m_start, dm
+    return m_start, dm, n_start, dn
+
+@Profile(stats=PERFORMANCE_STATS)
 def clamp(n, smallest, largest):
     return max(smallest, min(n, largest))
 
+@Profile(stats=PERFORMANCE_STATS)
 def tile_loss(m_start, dm, n_start, dn, level, l):
     # down-level if the width is exactly the level tile size, this is 
     # needed to ensure the xor_arr calculation works
     if dm == 8**level and level > 0:
         level -= 1    
     # ensure width is always bigger than height
-    if dn > dm:
-        m_start, dm, n_start, dn = n_start, dn, m_start, dm
+    m_start, dm, n_start, dn = width_bigger_than_height(m_start, dm, n_start, dn)
     # calculate the xor sum values and the boundaries
     if level == 0:
         m_values = range(m_start, m_start + 8)
@@ -182,6 +194,7 @@ def tile_loss(m_start, dm, n_start, dn, level, l):
                             m_start ^ n_start, level)
     return loss
 
+@Profile(stats=PERFORMANCE_STATS)
 def sum_seq(a_1, a_n):
     """
     Calculate sum of arithmetic sequence.
@@ -190,7 +203,8 @@ def sum_seq(a_1, a_n):
     return int((n * (a_1 + a_n)) / 2)
 
 def loss_array(m_s, m_e, n_s, n_e, l, t):
-    rows, cols = np.array(np.meshgrid(np.arange(n_s, n_e, dtype=object), np.arange(m_s, m_e, dtype=object)))
+    rows, cols = np.array(np.meshgrid(np.arange(n_s, n_e, dtype=object),
+                                      np.arange(m_s, m_e, dtype=object)))
     xor_arr = np.bitwise_xor(rows, cols)
     trans_loss = np.subtract(xor_arr, l)
     trans_loss[trans_loss < 0] = 0
@@ -199,4 +213,5 @@ def loss_array(m_s, m_e, n_s, n_e, l, t):
     return np.sum(xor_arr), loss, donate_time
 
 if __name__ == "__main__":
-    print(elder_age(328, 334, 12, 27974)) # 13932
+    print(elder_age(28827050410, 35165045587, 7109602, 13719506)) # 5456283
+    print(PERFORMANCE_STATS)
