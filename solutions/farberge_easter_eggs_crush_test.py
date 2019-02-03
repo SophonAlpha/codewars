91761df69d10c6fc88e67533207a7a5c7a033e80
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-03 09:03:03 +0400

    possible approach
    max floor seems to be 2^(max attempts) and then this scaled down to 1
    attempt

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -8,8 +8,5 @@ Level: 3 kyu
 
 def next_floor(floor, eggs, attempts):
     max_attempts = min(eggs, attempts)
-    
-    
-    
-    before_last_floor = floor + (attempts - eggs - 1)
-    return before_last_floor + (before_last_floor - floor)
+    all_fail_floors = [2**attempt for attempt in range(max_attempts)]
+    return floor + 2**(max_attempts - 1)
