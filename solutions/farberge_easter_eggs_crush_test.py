1816ff81d0883bdb001ad388d04a2a3a14b03cc0
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-03 09:04:51 +0400

    update

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -9,4 +9,4 @@ Level: 3 kyu
 def next_floor(floor, eggs, attempts):
     max_attempts = min(eggs, attempts)
     all_fail_floors = [2**attempt for attempt in range(max_attempts)]
-    return floor + 2**(max_attempts - 1)
+    return floor + 2**(max_attempts - 1) 
