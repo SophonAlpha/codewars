2336b7c6b34b7b1768df7fa702e99849ef69c520
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-07 09:30:25 +0400

    test approach

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -13,26 +13,8 @@ def height(eggs, tries):
         eggs_left = eggs - attempt
         attempts_left = tries - attempt
         if eggs_left == 1:
-            floor += attempts_left + 1
+            floor += attempts_left
         else:
             # recursive call
             pass
         print(floor, attempt, eggs_left, attempts_left)
-
-def get_floor(floor, attempt, eggs_left, attempts):
-    floor = floor
-
-
-#     
-#     max_attempts = min(eggs, tries)
-#     floor_before_last_attempt_all_fail = tries - (max_attempts - 1)
-#     
-#     
-#     
-#     floor = 0
-#     while tries > 0 and eggs > 0:
-#         max_attempts = min(eggs, tries)
-#         floor += 2**(max_attempts - 1)
-#         print(floor)
-#         tries -= 1
-#     return floor
