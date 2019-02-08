c6f4cac3b1eb0396121ee325150d6db21311bfb8
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-08 22:27:24 +0400

    test approach

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -6,15 +6,12 @@ Level: 3 kyu
 
 """
 
-def height(eggs, tries):
-    floor = 0
+def height(floor, eggs, tries):
     max_tries = min(eggs, tries)
-    for attempt in range(max_tries, 0, -1):
-        eggs_left = eggs - attempt
-        attempts_left = tries - attempt
-        if eggs_left == 1:
-            floor += attempts_left
-        else:
-            # recursive call
-            pass
-        print(floor, attempt, eggs_left, attempts_left)
+    eggs_left = eggs - max_tries
+    attempts_left = tries - max_tries
+    floor += attempts_left + 1
+    height(floor, eggs_left + 1, attempts_left)
+    return floor
+
+print(height(0, 4, 5))
