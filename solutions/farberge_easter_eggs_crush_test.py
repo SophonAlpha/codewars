fc98333fb3a06a26eb597af05936bf8997ad04e5
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-22 23:50:25 +0400

    recursive tree
    need to figure out how to compute the number of nodes in this tree structure

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -31,6 +31,21 @@ def get_floor(tower, recur_lvl, floor, attempt, eggs, tries):
         floor += attempts_left + 1
     else:
         for cur_attempt in range(attempt + 1, tries + 1):
+            if cur_attempt == attempt + 1:
+                tower, floor = get_floor(tower, recur_lvl, floor, cur_attempt, eggs - 1, tries)
+            else:
+                
+            tower.insert(floor, (recur_lvl, cur_attempt, eggs, tries - cur_attempt))
+        floor += 1
+    return tower, floor
+
+def get_floor_v2(tower, recur_lvl, floor, attempt, eggs, tries):
+    recur_lvl += 1
+    attempts_left = tries - attempt
+    if eggs <= 1 or attempts_left <= 1:
+        floor += attempts_left + 1
+    else:
+        for cur_attempt in range(attempt + 1, tries + 1):
             tower, floor = get_floor(tower, recur_lvl, floor, cur_attempt, eggs - 1, tries)
             tower.insert(floor, (recur_lvl, cur_attempt, eggs, tries - cur_attempt))
         floor += 1
