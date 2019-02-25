f02143b5c372033c7710fcb843b588da80d71400
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-25 23:23:54 +0400

    add floor calculation

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -10,28 +10,54 @@ Level: 3 kyu
 import pprint
 
 def height(eggs, tries):
-    tower = [('-', '-', '-', '-')] * 1000
-    tower.insert(0, (0, 0, eggs, tries))
     recur_lvl = 0
     floor = 0
     attempt = 0
     if eggs > 0 and tries > 0:
-        get_floor(tower, recur_lvl, floor, attempt, eggs, tries)
+        floor = get_floor(recur_lvl, floor, attempt, eggs, tries)
+        print()
+        floor = get_floor_v1(recur_lvl, floor, attempt, eggs, tries) - 1
+        print(floor)
     return floor
 
-def get_floor(tower, recur_lvl, floor, attempt, eggs, tries):
+def get_floor(recur_lvl, floor, attempt, eggs, tries):
+    floor = 0
     segments = max(eggs, tries)
-    max_recurse = min(eggs, tries) - 1
-    level_count = {1: [1] * segments}
-    for recurse_lvl in range(2, max_recurse + 1):
-        segments = segments - 1
+    max_level = min(eggs, tries) - 1
+    level_count = {}
+    for level in range(1, max_level + 1):
         for segment in range(segments):
-            start = segment if segment < segments - 2 else segments - 2
-            segment_count = sum(level_count[recurse_lvl - 1][start:: -1])
-            if recurse_lvl not in level_count:
-                level_count[recurse_lvl] = []
-            level_count[recurse_lvl].append(segment_count)
+            attempt = segment + level
+            if level > 1:
+                start = segment if segment < segments - 2 else segments - 2
+                segment_count = sum(level_count[level - 1][start:: -1])
+            else:
+                segment_count = 1
+            if level not in level_count:
+                level_count[level] = []
+            level_count[level].append(segment_count)
+            if eggs - attempt <= 1 or tries - attempt <= 1:
+                floor_height = tries - attempt + 1
+            else:
+                floor_height = 1
+            floor += floor_height
+        segments -= 1
     pprint.pprint(level_count)
-    return
+    print(floor)
+    return floor
+
+def get_floor_v1(recur_lvl, floor, attempt, eggs, tries):
+    recur_lvl += 1
+    attempts_left = tries - attempt
+    if eggs <= 1 or attempts_left <= 1:
+        floor += attempts_left + 1
+    else:
+        for cur_attempt in range(attempt + 1, tries + 1):
+            floor = get_floor_v1(recur_lvl, floor, cur_attempt, eggs - 1, tries)
+            print('lvl: {}, floor: {}, attempt: {}'.format(recur_lvl, floor, cur_attempt))
+        floor += 1
+    return floor
 
 height(5, 6)
+print()
+# height(7, 20) # 137979
