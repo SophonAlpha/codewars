690fa07726f5146eba37972d8fdbe2615bae61ae
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-02-28 09:14:33 +0400

    optimisation: only store quantity of of segments for previous and current level

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -17,30 +17,30 @@ def get_floor(eggs, tries):
     floor = 0
     segments = tries
     max_level = min(eggs, tries)
-    level_count = {}
+    level_count_prev = []
     for level in range(1, max_level):
+        level_count_curr = []
         for segment in range(segments):
             attempt = segment + level
             if level > 1:
                 start = segment if segment < segments - 2 else segments - 2
-                segment_count = sum(level_count[level - 1][start:: -1])
+                segment_count = sum(level_count_prev[start:: -1])
             else:
                 segment_count = 1
-            if level not in level_count:
-                level_count[level] = []
-            level_count[level].append(segment_count)
+            level_count_curr.append(segment_count)
             if eggs - 1 <= 1 or tries - attempt <= 1:
                 floor_height = tries - attempt + 1
             else:
                 floor_height = 1
             floor += floor_height * segment_count
+        level_count_prev = level_count_curr[:]
         segments -= 1
         eggs -= 1
     return floor
 
 if __name__ == "__main__":
-#     print(height(5, 6))
-#     print(height(19, 3))
+    print(height(5, 6))
+    print(height(19, 3))
     # print(height(5, 6)) # 62
     # print(height(7, 20)) # 137979
-    print(height(477, 500))
+#     print(height(477, 500))
