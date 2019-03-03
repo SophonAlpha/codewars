351e0a73f2e52a2d3f8c75b88cdc35326601b27a
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-03-03 22:15:16 +0400

    new approach needed, Catalan's trapezoid to slow

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -12,6 +12,8 @@ import math
 def height(eggs, tries):
     floor = 0
     if eggs > 0 and tries > 0:
+        eggs = eggs if eggs <= tries else tries
+#         floor = calculate_Catalan_sums(eggs, tries)
         floor = get_floor(eggs, tries)
     return floor
 
@@ -30,37 +32,31 @@ def get_floor(eggs, tries):
                 segment_count = 1
             level_count_curr.append(segment_count)
             floor += segment_count
-        print('level {}: {} = {}'.format(level, level_count_curr, sum(level_count_curr)))
+#         print('level {}: {} = {}'.format(level, level_count_curr, sum(level_count_curr)))
+        print('level {}: {}'.format(level, sum(level_count_curr)))
+#         print('floor = {}',format(floor))
         level_count_prev = level_count_curr[:]
         segments -= 1
         eggs -= 1
     return floor
 
-def sum_arith_seq(num, start, end):
-    return (num * (start + end)) / 2
-
-def transform_Catalans_tri(levels, segments):
+def calculate_Catalan_sums(levels, segments):
+    level_sum = 0
     for level in range(1, levels + 1):
-        level_sum = int(Catalans_trapezoid(level, segments - level))
-        print('level {}: {}'.format(level, level_sum))
-    return
-
-def Catalans_triangle(n, k):
-    c = (math.factorial(n + k) * (n - k + 1)) / (math.factorial(k) * math.factorial(n + 1))
-    return c
+        level_sum += Catalans_trapezoid(level, segments - level)
+    return level_sum
 
 def Catalans_trapezoid(n, k):
     """
     Generalisation of Catalan's triangle = Catalan's trapezoid
     https://en.wikipedia.org/wiki/Catalan%27s_triangle
     """
-    c = (math.factorial(n + k) / math.factorial(n)) / math.factorial(k)
+    c = (math.factorial(n + k) // math.factorial(n)) // math.factorial(k)
     return c
 
 if __name__ == "__main__":
-
-    print(height(9, 9))
+    print(height(7, 8))
     print()
-    transform_Catalans_tri(9, 9)
-#     print(height(5, 7))
-#     print()
+    print(height(7, 9))
+    print()
+
