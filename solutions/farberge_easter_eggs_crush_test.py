d3367c2fa80d8e4b669f2634f5ed81393fa6e8d9
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-04-06 17:57:48 +0400

    totals triangle approach
    Fixed out of memory problem for large values but still not fast enough.

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -13,15 +13,17 @@ def height(eggs, tries):
         eggs = eggs if eggs <= tries else tries
         floor = get_floor(eggs, tries)
         if not floor:
-            lookup = {(sub_eggs, sub_eggs + 2): get_floor(sub_eggs, sub_eggs + 2) 
-                      for sub_eggs in range(1, eggs + 1)}
-            lookup.update({(1, sub_tries): sub_tries for sub_tries in range(4, tries + 1)})
+            cur_lookup = {(sub_eggs, sub_eggs + 2): get_floor(sub_eggs, sub_eggs + 2) 
+                          for sub_eggs in range(1, eggs + 1)}
             for lane in range(tries - (eggs + 2)):
+                new_lookup = {}
+                new_lookup[(1, lane + 4)] = lane + 4
                 for sub_eggs in range(2, eggs + 1):
                     sub_tries = lane + sub_eggs + 3
-                    lookup[(sub_eggs, sub_tries)] = lookup[(sub_eggs, sub_tries - 1)] + \
-                                                    lookup[(sub_eggs - 1, sub_tries - 1)] + 1
-            floor = lookup[(eggs, tries)]
+                    new_lookup[(sub_eggs, sub_tries)] = cur_lookup[(sub_eggs, sub_tries - 1)] + \
+                                                        new_lookup[(sub_eggs - 1, sub_tries - 1)] + 1
+                cur_lookup = new_lookup.copy()
+            floor = new_lookup[(eggs, tries)]
     return floor
 
 def get_floor(eggs, tries):
@@ -40,4 +42,6 @@ def get_floor(eggs, tries):
     return floor
 
 if __name__ == "__main__":
-    print(height(5, 6))
+#     print(height(10, 10))
+#     print(height(477, 3200))
+    print(height(5, 11))
