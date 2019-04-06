6b4d7a95720c0aa9bead84c3238e56881e3a4aeb
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-04-07 01:56:08 +0400

    code cleanup

--- solutions/farberge_easter_eggs_crush_test.py
+++ solutions/farberge_easter_eggs_crush_test.py
@@ -7,21 +7,6 @@ Level: 3 kyu
 
 """
 
-import math
-import pprint
-
-def show_matrix(max_eggs, max_tries):
-    results = []
-    for eggs in range(0, max_eggs + 1):
-        results_row = []
-        for tries in range(0, max_tries + 1):
-#             eggs_2 = eggs if eggs <= tries  else tries
-            results_row.append(Catalans_trapezoid(eggs, tries))
-        results.append(results_row)
-#     pprint.pprint(results_1)
-#     print()
-    pprint.pprint(results)
-
 def height(eggs, tries):
     floor = 0
     if eggs > 0 and tries > 0:
@@ -29,17 +14,6 @@ def height(eggs, tries):
         floor = get_floor(eggs, tries)
         if not floor:
             floor = max_floor(eggs, tries)
-#             cur_lookup = {(sub_eggs, sub_eggs + 2): get_floor(sub_eggs, sub_eggs + 2) 
-#                           for sub_eggs in range(1, eggs + 1)}
-#             for lane in range(tries - (eggs + 2)):
-#                 new_lookup = {}
-#                 new_lookup[(1, lane + 4)] = lane + 4
-#                 for sub_eggs in range(2, eggs + 1):
-#                     sub_tries = lane + sub_eggs + 3
-#                     new_lookup[(sub_eggs, sub_tries)] = cur_lookup[(sub_eggs, sub_tries - 1)] + \
-#                                                         new_lookup[(sub_eggs - 1, sub_tries - 1)] + 1
-#                 cur_lookup = new_lookup.copy()
-#             floor = new_lookup[(eggs, tries)]
     return floor
 
 def get_floor(eggs, tries):
@@ -57,32 +31,14 @@ def get_floor(eggs, tries):
         floor = None
     return floor
 
-def Catalans_trapezoid(n, k):
-    """
-    Generalisation of Catalan's triangle = Catalan's trapezoid
-    https://en.wikipedia.org/wiki/Catalan%27s_triangle
-    """
-    c = (math.factorial(n + k) // math.factorial(n)) // math.factorial(k)
-    return c
-
 def max_floor(eggs, tries):
     floor = 0
-    aux = 1
+    temp_val = 1
     for i in range(1, eggs + 1):
-        aux *= tries + 1 - i
-        aux //= i
-        floor += aux
+        temp_val *= tries + 1 - i
+        temp_val //= i
+        floor += temp_val
     return floor
-
-def binomial_coefficient(n, k):
-    """
-    http://mathworld.wolfram.com/BinomialCoefficient.html
-    """    
-    c = math.factorial(n) // (math.factorial(n - k) * math.factorial(k))
-    return c
 
 if __name__ == "__main__":
     print(height(2, 14))
-#     print(height(477, 3200))
-#     show_matrix(10,10)
-#     print(height(5, 11))
