1ab1ca97ea4c0c4ca0c23ee258f43e192581e748
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-06-10 22:23:23 +0400

    cleanup
    removed unused code

--- solutions/break_the_pieces_evilized_edition.py
+++ solutions/break_the_pieces_evilized_edition.py
@@ -221,36 +221,6 @@ def get_next_direction(directions):
             direction = val
             break
     return direction
-
-def break_evil_pieces_v1(shape):
-    shape_lines = shape.split('\n')
-    pattern = re.compile(r'(?=(?P<line>[\+\|]-*?[\+\|]))'
-                         r'|(?=(?P<segment>[\+\|] *?[\+\|]))')
-    pieces = []
-    for line_idx, shape_line in enumerate(shape_lines):
-        for match in pattern.finditer(shape_line):
-            segment = Segment(match)
-            pieces = segment_to_piece(segment, line_idx, pieces)
-    return
-
-def segment_to_piece(segment, line_idx, pieces):
-    for pos, piece in enumerate(pieces):
-        if piece[-1]['start'] <= segment.start <= piece[-1]['end'] and \
-           piece[-1]['start'] <= segment.end <= piece[-1]['end']:
-            pieces[pos].append({'line': line_idx,
-                                'start': segment.start,
-                                'end': segment.end,
-                                'string': segment.string})
-    if segment.type == 'line':
-        pieces = add_piece(segment, line_idx, pieces)
-    return pieces
-
-def add_piece(segment, line_idx, pieces):
-    pieces.append([{'line': line_idx,
-                    'start': segment.start,
-                    'end': segment.end,
-                    'string': segment.string}])
-    return pieces
     
 if __name__ == '__main__':
     shape = """
