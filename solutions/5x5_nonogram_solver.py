3bcce1f824f792addbb0ff2d962b2c73f0fc00bd
SophonAlpha <SophonAlpha@users.noreply.github.com>
2020-02-10 09:20:41 +0400

    sketching binary or operation across multiple integer, display binary number with same length

--- solutions/5x5_nonogram_solver.py
+++ solutions/5x5_nonogram_solver.py
@@ -7,11 +7,19 @@ Level: 4 kyu
 try nonograms here: https://www.puzzle-nonograms.com/
 """
 
+import functools
 
 done = False
 squares = [1, 2]
-runs = [0, 0, 0, 0, 0]
+run_len = 5
 variants = set()
+
+a = [1, 2, 4, 8, 16, 32, 64, 128]
+b = functools.reduce(lambda x, y: x | y, a)
+fmt = '{0:0' + str(run_len) + 'b}'
+print(fmt.format(b))
+
+
 
 while not done:
     max_pos = len(runs) - (sum(squares) + len(squares) - 1)
